1cfc41511b48ee48c202cae7419b1bc34279d536
redpalico <142068138+redpalico@users.noreply.github.com>
2023-08-27 11:41:42 +0000

    saikoro

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // ランダムな1から6の整数を生成
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  // レスポンスの設定
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(randomNumber.toString()); // 数字を文字列として送信
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
